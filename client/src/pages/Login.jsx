57be04a21a0d02d173edae91a0498e4e8bdec6c6
Abhijeet Tiwari <abhijeett709@gmail.com>
2025-06-28 15:14:38 +0530

    Changes in the file such as : Login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -8,22 +8,39 @@ function Login() {
   const [password, setPassword] = useState("");
   const [bio, setBio] = useState("");
   const [isDateSubmitted, setIsDataSubmitted] = useState(false);
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    if (currState === "Sign up" && !isDateSubmitted) {
+      setIsDataSubmitted(true);
+      return;
+    }
+  };
   return (
     <div className="min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl">
       {/* Left */}
       <img src={assets.logo_big} alt="" className="w-[min(30vw,250px)]" />
       {/* Right */}
-      <form className="border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg">
+      <form
+        onSubmit={onSubmitHandler}
+        className="border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg"
+      >
         <h2 className="font-medium text-2xl flex justify-between items-center">
           {currState}
-          <img src={assets.arrow_icon} alt="" className="w-5 cursor-pointer" />
+          {isDateSubmitted && (
+            <img
+              onClick={() => setIsDataSubmitted(false)}
+              src={assets.arrow_icon}
+              alt=""
+              className="w-5 cursor-pointer"
+            />
+          )}
         </h2>
         {currState === "Sign up" && !isDateSubmitted && (
           <input
             onChange={(e) => setFullName(e.target.value)}
             value={fullName}
             type="text"
-            className="p-2 border-gray-500 rounded-md focus:outline-none"
+            className="p-2 border border-gray-500 rounded-md focus:outline-none"
             placeholder="Full Name"
             required
           />
@@ -48,12 +65,52 @@ function Login() {
             />
           </>
         )}
-        {currState === "Signup" && isDateSubmitted && (
+        {currState === "Sign up" && isDateSubmitted && (
           <textarea
-            rwos={4}
+            onChange={(e) => setBio(e.target.value)}
+            value={bio}
+            rows={4}
             className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-indigo-500"
+            placeholder="provide a short bio ..."
+            required
           ></textarea>
         )}
+        <button
+          type="submit"
+          className="py-3 bg-gradient-to-r from-purple-400 to-violte-600 text-white rounded-md cursor-pointer"
+        >
+          {currState === "Sign up" ? "Create Account" : "Login Now"}
+        </button>
+        <div className="flex items-center gap-2 text-sm text-gray-500">
+          <input type="checkbox" />
+          <p>Agree to the terms of use & privacy policy.</p>
+        </div>
+        <div className="flex flex-col gap-2">
+          {currState === "Sign up" ? (
+            <p className="text-sm text-gray-600">
+              Already have an account ?{" "}
+              <span
+                onClick={() => {
+                  setCurrState("Login");
+                  setIsDataSubmitted(false);
+                }}
+                className="font-medium text-violet-500 cursor-pointer"
+              >
+                Login here
+              </span>
+            </p>
+          ) : (
+            <p className="text-sm text-gray-600">
+              Create an account{" "}
+              <span
+                onClick={() => setCurrState("Sign up")}
+                className="font-medium text-violet-500 cursor-pointer"
+              >
+                Click here
+              </span>
+            </p>
+          )}
+        </div>
       </form>
     </div>
   );
