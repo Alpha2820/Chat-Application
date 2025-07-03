b0044f4fb1349579fb15a40d92b262fbbd9704c3
Abhijeet Tiwari <abhijeett709@gmail.com>
2025-07-03 12:31:07 +0530

    Some Changes Made

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -32,3 +32,32 @@ export const signup = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+// Controller to login a user
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userData = await User.findOne({ email });
+    const isPasswordCorrect = await bcrypt.compare(password, userData.password);
+    if (!isPasswordCorrect) {
+      return res.json({ success: false, message: "Invalid credentials" });
+    }
+    const token = generateToken(userData._id);
+    res.json({ success: true, userData, token, message: "Login successful" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+// Controller to check if user is authenticated
+export const checkAuth = (req, res) => {
+  res.json({ success: true, user: req.user });
+};
+
+// Controller to update user profile details
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic, bio, fullName } = req.body;
+    const userId = req.user._id;
+  } catch (error) {}
+};
